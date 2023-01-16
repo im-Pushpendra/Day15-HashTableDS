959f3a0e17efe925a80f75794a8da773c2fbd4e0
im-Pushpendra <112095599+im-Pushpendra@users.noreply.github.com>
2023-01-16 20:00:46 +0530

    Hash Tables

---
+++ HashTable.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.day15.UC_Day15_HashTableDS;
+
+public class HashTable {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Hash Tables");
+
+	}
+
+}
